d3f6b3a638caac41ad6633d3481bf1d13efa5d5c
ShivanshKumarJha <kumarjhashivansh@gmail.com>
2024-12-19 02:05:41 +0530

    Learned about modules

---
+++ Python/ModulesAndPackages/main.py
@@ -0,0 +1,34 @@
+# import calculator as c
+
+# print(type(c))
+# print(c.add(1,2))
+# print(c.sub(1,2))
+# print(c.mul(12,2))
+# print(c.div(12,2))
+
+# from calculator import add, sub
+# from calculator import *
+
+# print(add(1,2))
+# print(sub(1,2))
+
+# from calculator import (
+# 	add,
+# 	sub,
+# 	a,
+# 	b,
+# 	c,
+# 	d
+# )
+
+# print(a)
+# print(add(1,2))
+
+# Usage of Package
+from maths import (
+	simple,
+	complex
+)
+
+print(simple.add(1,2))
+print(complex.square(3))
---
+++ Python/ModulesAndPackages/maths/__init__.py
@@ -0,0 +1,2 @@
+# This file specify that this folder is a package - means collection of modules
+__all__ = ['simple','complex']
